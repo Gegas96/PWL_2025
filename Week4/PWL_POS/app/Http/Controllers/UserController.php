4d258a4b51deca670a559963df7e9ed3393baeb6
Gegas96 <gegasanugrah@gmail.com>
2025-03-10 20:20:04 +0700

    Praktikum-2.4

--- Week4/PWL_POS/app/Http/Controllers/UserController.php
+++ Week4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,18 +10,15 @@ class UserController extends Controller
 {
     public function index() {
 
-        // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'name' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-
-        // UserModel::create($data); // update data user
-
-        // coba akses model UserModel
-        $user = UserModel::where('level_id', 2)->count(); // ambil semua data dari tabel m_user
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'name' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user', ['data' => $user]);
     }
 }
--- Week4/PWL_POS/resources/views/user.blade.php
+++ Week4/PWL_POS/resources/views/user.blade.php
@@ -9,11 +9,13 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>Id Level Pengguna</th>
            
         </tr>
             <tr>
-                <td>{{ $data }}</td>
                 <td>{{ $data->user_id }}</td>
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->name }}</td>
