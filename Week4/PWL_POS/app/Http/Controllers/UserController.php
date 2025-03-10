8b79d0431da200f2c446b0a7fd77feb67668285a
Gegas96 <gegasanugrah@gmail.com>
2025-03-10 16:36:44 +0700

    Praktikum-2.1

--- Week4/PWL_POS/app/Http/Controllers/UserController.php
+++ Week4/PWL_POS/app/Http/Controllers/UserController.php
@@ -11,17 +11,19 @@ class UserController extends Controller
     public function index() {
 
         // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'name' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'name' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
 
-        UserModel::create($data); // update data user
+        // UserModel::create($data); // update data user
 
         // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::findOr(20, ['username', 'name'], function() {
+            abort(404);
+        }); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
