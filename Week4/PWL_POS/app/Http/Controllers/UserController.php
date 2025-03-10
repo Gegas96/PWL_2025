694dd930e749fc730250f4643102adef2e13b492
Gegas96 <gegasanugrah@gmail.com>
2025-03-10 16:39:53 +0700

    Praktikum-2.2

--- Week4/PWL_POS/app/Http/Controllers/UserController.php
+++ Week4/PWL_POS/app/Http/Controllers/UserController.php
@@ -21,9 +21,7 @@ class UserController extends Controller
         // UserModel::create($data); // update data user
 
         // coba akses model UserModel
-        $user = UserModel::findOr(20, ['username', 'name'], function() {
-            abort(404);
-        }); // ambil semua data dari tabel m_user
+        $user = UserModel::where('username', 'manager9')->firstOrFail(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
