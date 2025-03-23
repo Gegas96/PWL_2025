e7b4c288be13b8c157e4b4a7970681427800eb4d
Gegas96 <gegasanugrah@gmail.com>
2025-03-23 13:53:31 +0700

    Praktikum-4

--- Week5/PWL_POS/app/Http/Controllers/UserController.php
+++ Week5/PWL_POS/app/Http/Controllers/UserController.php
@@ -24,13 +24,20 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = levelModel::all(); // ambil data level untuk filter
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables 
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'name', 'level_id')->with('level');
+
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             ->addIndexColumn() // Menambahkan kolom index otomatis
